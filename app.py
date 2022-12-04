ec4ebf070c5d4f6e669bff4ee020980530fb4d31
Vishal Bende <vishal@prmsnls.xyz>
2022-12-04 20:43:49 +0530

    fix: cors

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 import os
 import openai
+import flask
   
 # creating a Flask app
 app = Flask(__name__)
-# CORS(app, resources={r"/*": {"origins": "*"}}, allow_headers="*")
-CORS(app, support_credentials=True)
-# app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app, resources={r"/*": {"origins": "*"}}, allow_headers="*")
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 openai.organization = os.getenv('organisation')# "org-VgYqHHC2seYeXwoHsU05U3yT"
 openai.api_key = os.getenv('api') #
@@ -18,7 +18,7 @@ openai.api_key = os.getenv('api') #
 # returns hello world when we use GET.
 # returns the data that we send when we use POST.
 @app.route('/', methods = ['POST'])
-@cross_origin(origin='*', support_credentials=True, allow_headers=True, methods=["POST"])
+@cross_origin(origin='*', support_credentials=True, allow_headers=True)
 def home():
     request_data = request.json
     prompt = request_data.get('prompt')
@@ -28,7 +28,10 @@ def home():
         prompt=prompt,
         **dict(config)
     )
-    return(res)
+
+    response = flask.jsonify(res)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
   
 
 
