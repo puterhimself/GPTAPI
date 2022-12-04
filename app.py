e80768ac3afef0861c9e4d6334db17c8f0960d23
Vishal Bende <vishal@prmsnls.xyz>
2022-12-04 20:52:31 +0530

    fix: cors

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ openai.api_key = os.getenv('api') #
 # returns hello world when we use GET.
 # returns the data that we send when we use POST.
 @app.route('/', methods = ['POST'])
-@cross_origin(origin='*', support_credentials=True, allow_headers=True)
+@cross_origin()
 def home():
     request_data = request.json
     prompt = request_data.get('prompt')
